6a79d5b8a68165ad5ddf748e96e0c96f4db601ae
Anushka Prajapati <anushkaprajapati9898@gmail.com>
2026-05-23 16:39:27 +0530

    Chapter:-1

---
+++ variable.js
@@ -0,0 +1,20 @@
+FullName = "Anushka"
+Age=20
+Price=120.09
+x=null;
+y=undefined;
+console.log(FullName)
+console.log(Age)
+console.log(Price)
+
+
+// This through also are run this code you can also use this formate code 
+
+// FullName = "Anushka";
+// Age=20;
+// Price=120.09;
+// x=null;  [Here Null means are known but here blank space]
+// y=undefined;  [Here Undefined don't known]
+// console.log(FullName);
+// console.log(Age);
+// console.log(Price);
