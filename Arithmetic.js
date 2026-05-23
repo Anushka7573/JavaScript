e944c8057e0841aa6147409002500a9e22553400
Anushka Prajapati <anushkaprajapati9898@gmail.com>
2026-05-23 16:40:43 +0530

    chapter:-2

---
+++ Arithmetic.js
@@ -0,0 +1,47 @@
+// Arithmetic Operator ------------------------------
+
+let a = 2, b = 3;
+
+/*
+console.log("a=",a,"&b=",b);
+ console.log("a+b =",a+b);
+ console.log("a/b =",a/b);
+ console.log("a-b =",a-b);
+ console.log("a*b =",a*b);
+ console.log("a%b =",a%b);
+ console.log("a**b =",a**b);
+*/
+
+/*
+--Unary Operator------------------------------- 
+
+are two type for increment(++) and decrement(--)
+Here two way how to write this operator code
+
+console.log("a=", a, "&b=", b);
+
+Here same time to we can follow this code due to ,
+1st way & 2nd way on the time both answer are will be not same seen them.
+
+1st way
+a= a-1;
+console.log("a=",a);
+
+2nd way
+a--;
+console.log("a=",a);
+*/
+
+
+/* 
+++a are (pre) this means are pre for before work [Pehle kaam hoga]
+*/
+// console.log("a=", a, "&b=", b);
+// console.log("++a=", ++a);
+
+/* 
+a++ are (post) this means are pre for after work [baad may kaam hoga]
+*/
+console.log("a=", a, "&b=", b);
+console.log("a++=", a++);
+console.log("a=",a);
---
+++ comment.js
@@ -0,0 +1,4 @@
+// Print Hello World. This is only apply for single line 
+/*Print Hello World.  This is only apply for single line*/
+
+console.log("Hello World");
